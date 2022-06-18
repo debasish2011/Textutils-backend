b0b9bc79438817b53d6daf2a2ca188ee6b9d6264
Debasish <debasish20071999@gmail.com>
2022-06-18 15:29:24 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import './App.css';
 
+import Form from "./components/Form";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar title='Textutils'/>
+      <Form/>
     </>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Form(props) {
+  return (<>
+    <input type="email" className="input"/>
+  </>);
+}
