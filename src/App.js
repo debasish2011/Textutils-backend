7bc69eb79b8df47b7aed584072d515504ffffc43
debasish2011 <92264640+debasish2011@users.noreply.github.com>
2022-07-21 16:37:09 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -8,17 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="column_flex container">
-        <h2>
-          <label htmlFor="exampleFormControlTextarea1" className="form-label">
-            Enter text to analyse
-          </label>
-        </h2>
-          <Form/>
-      </div>
-      <div className="flex container btnarea">
-        
-      </div>
+      <Form />
     </>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,17 @@ import "../css/form.css";
 export default function Form() {
   return (
     <>
-      <textarea id="form" cols="30" rows="10"></textarea>
+      <div className="column_flex container">
+        <h2>
+          <label htmlFor="form" className="form-label">
+            Enter text to analyse
+          </label>
+        </h2>
+        <textarea id="form" cols="30" rows="10"></textarea>
+      </div>
+      <div className="flex container btnarea">
+        
+      </div>
     </>
   );
 }
