5e225ca09e987b7fb2be5cebaf1e91a454e93538
debasish2011 <92264640+debasish2011@users.noreply.github.com>
2022-07-21 17:11:06 +0530

    ok

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/form.css";
 
 export default function Form() {
+  const [text, setText] = useState("Enter text here");
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const changeToUppercase = () => {
+    setText(text.toUpperCase());
+  };
+  const changeToLowercase = () => {
+    setText(text.toLowerCase());
+  };
   return (
     <>
       <div className="column_flex container">
@@ -10,10 +20,21 @@ export default function Form() {
             Enter text to analyse
           </label>
         </h2>
-        <textarea id="form" cols="30" rows="10"></textarea>
+        <textarea
+          id="form"
+          cols="30"
+          rows="10"
+          value={text}
+          onChange={handleOnChange}
+        ></textarea>
       </div>
       <div className="flex container btnarea">
-        
+        <button className="btn btn-blue" onClick={changeToUppercase}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-blue" onClick={changeToLowercase}>
+          Convert to Lowercase
+        </button>
       </div>
     </>
   );
